fde88e5b99f47188d37319cd0830bd4f73d173b2
SebastianTre <sebastian-t@mein.gmx>
2024-11-05 13:33:22 +0100

    mitarbeiter view und mitarbeitermodell eingefügt

---
+++ src/main/java/org/example/sebitest123/mitarbeiter.java
@@ -0,0 +1,31 @@
+package org.example.sebitest123;
+
+public class mitarbeiter {
+    private int m_id;
+    private String name;
+    private String vorname;
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+}
